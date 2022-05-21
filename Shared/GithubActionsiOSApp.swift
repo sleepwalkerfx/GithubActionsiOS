d8bb6bf607b150e14b599e30798ce0aeb06ec872
Rukshan Marapana <rukshan47@gmail.com>
2022-05-21 19:30:47 +0530

    Initial Commit

---
+++ Shared/GithubActionsiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsiOSApp.swift
+//  Shared
+//
+//  Created by Rukshan Marapana on 2022-05-21.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
